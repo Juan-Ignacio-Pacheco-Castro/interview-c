08355f5e0c7b1f1a0674eb4292f224b5f7aa4c43
Juan Ignacio Pacheco Castro <juanipacas@gmail.com>
2023-10-06 11:48:26 -0600

    Scanf reads until first white space changed to first enter or 99 letters

--- ctci12.2/reverseString.c
+++ ctci12.2/reverseString.c
@@ -4,16 +4,17 @@
 int main() {
    char line[100];
    printf("Type a line: \n");
-   scanf("%s", &line);
-   reverse(&line);
-   printf("%s\n", &line);
+   scanf("%99[^\n]", line);
+   reverse(line);
+   printf("%s\n", line);
    return 0;
 }
 
 //Reverse String: Implement a function void reverse( char* str) in C or C++ which reverses a null-terminated string.
 void reverse(char* str){
+   int length = strlen(str);
    int start = 0;
-   int end = strlen(str) - 1;
+   int end = length - 1;
 
    char temp;
    while (start < end){
